936e6e7ebdfe3b1fcd98daec2b457b1f69ce2cef
Tanya <efita41@gmail.com>
2024-12-10 12:30:18 +0300

    replace id whith product

--- app.rb
+++ app.rb
@@ -24,10 +24,14 @@ end
 post '/cart' do
 	#@products = Product.all
 	orders_input = params[:orders]
-	@orders = parse_orders_input orders_input
+	@items = parse_orders_input orders_input
 
-	erb "Hello! #{@orders.inspect}"
-	#erb :cart
+	@items.each do |item|
+		 #id, cnt
+		 item[0] = Product.find(item[0])
+	end
+	#erb "Hello! #{@orders.inspect}"
+	erb :cart
 end
 
 def parse_orders_input orders
